6c433102352a6de714feeb5eb6feb5e0071020e4
ManuEsse <emmanuel.gc.rosemont@gmail.com>
2021-08-09 12:36:45 +0400

    Composer add & Stripe add

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f626b11617c149ddf895f3644ba1eac72109a003',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f626b11617c149ddf895f3644ba1eac72109a003',
+    ),
+    'stripe/stripe-php' => 
+    array (
+      'pretty_version' => 'v7.92.0',
+      'version' => '7.92.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4b549e6f7d3e7ffd877547a0f1e8bd01c363e268',
+    ),
+  ),
+);
